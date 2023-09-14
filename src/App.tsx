75a6eae8e96218cd0379f977ac8c2b2e91462493
korkost <koreikinvk@gmail.com>
2023-09-14 12:14:31 +0300

    функция для динамического  управления сss классами по каким либо условиям, поправил опечатки в названиях

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import { Route, Routes, Link, } from 'react-router-dom';
 import { Suspense, useContext, useState } from 'react';
 import './styles/index.scss';
-import { AboutPaageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPaageAsync } from './pages/MainPage/MainPage.async';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { useTheme } from './theme/useTheme';
+import { classNames } from './helpers/classNames/classNames';
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
    
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                   <Route path={'/about'} element={<AboutPaageAsync />} />
-                   <Route path={'/'} element={<MainPaageAsync />}/>
+                   <Route path={'/about'} element={<AboutPageAsync />} />
+                   <Route path={'/'} element={<MainPageAsync />}/>
                 </Routes>
             </Suspense>
         </div>
