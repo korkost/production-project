f8cd7db3f32c18fb356e07fd5262078c9c3df038
korkost <koreikinvk@gmail.com>
2023-09-14 18:35:13 +0300

    поправили структуру проекта, перешди на архитектуру FSD, поправили настройку путей на абсолютные импорты

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import ThemeProvider from "./theme/ThemeProvider";
+import App from "app/App";
+import {ThemeProvider} from "app/providers/ThemeProvider";
 import {render} from "react-dom"
 
 render(
