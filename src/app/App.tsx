24a563d7de6911cd89d2e46335bedc1c8fe3f869
korkost <koreikinvk@gmail.com>
2023-09-15 17:48:10 +0300

    разработал первый виджет(навигационная понель), и первый UI компонент из UI KIT, добавил собственный снипет

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from './providers/router';
-import { Link } from 'react-router-dom';
+import { Navbar } from 'widgets/Navbar';
 
 
 const App = () => {
@@ -10,10 +10,9 @@ const App = () => {
    
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
+            <Navbar />
             <AppRouter />
+            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     );
 };
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,9 +1,12 @@
 type Mods = Record<string, boolean | string>;
 
-export function classNames(cls: string, mods: Mods, additional: string[]): string {
+// cls: string, mods?: Mods, additional?: string[]
+// второй вариант сделать аргументы не обязательными
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
   return [
     cls,
-    ...additional,
+    // могут приходить undefained,фильтрация.
+    ...additional.filter(Boolean),
     ...Object.entries(mods)
       .filter(([className, value]) => Boolean(value))
       .map(([className, value]) => className),
