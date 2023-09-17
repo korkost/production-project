f6f1ecdc4195568183c9eb1b56ca9a45b2de41c0
korkost <koreikinvk@gmail.com>
2023-09-17 15:20:18 +0300

    добавили байбл и экстракт плагин

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -10,6 +10,26 @@ const svgLoader = {
         use: ['@svgr/webpack'],
 }
 
+const babelLoader = {
+  test: /\.(js|jsx|tsx)$/,
+  exclude: /node_modules/,
+  use: {
+    loader: "babel-loader",
+    options: {
+      presets: ['@babel/preset-env'],
+      "plugins": [
+        [
+          "i18next-extract", 
+          {
+            locales: ['ru', 'en'],
+            keyAsDefaultValue: true
+          }
+        ]
+      ]
+    }
+  }
+}
+
 const cssLoader =  {
     test: /\.s[ac]ss$/i,
     use: [
@@ -46,6 +66,7 @@ const cssLoader =  {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
       ]
